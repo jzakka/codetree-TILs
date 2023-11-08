3a353d34aa301ca80ff600760e022b960bd2e521
Jzakka <105845911+jzakka@users.noreply.github.com>
2023-11-08 15:56:09 +0900

    Title: 우리 지금 만나 당장 만나, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 231108/우리 지금 만나 당장 만나/meet-now-right-now.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <queue>
+#include <iomanip>
+
+#define ll long long
+
+using namespace std;
+
+bool check(vector<pair<ll, ll>> info, ll time){
+  ll centre = info.front().first + info.front().second * time;
+
+  for (const auto &posAndVelo: info){
+    ll pos = posAndVelo.first;
+    ll velocity = posAndVelo.second;
+
+    if (abs(centre - pos) > velocity * time) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+void solution(vector<ll> &pos, vector<ll> &velocity){
+  vector<pair<ll, ll>> info(pos.size());
+  for (int i = 0; i < info.size(); ++i) {
+    info.at(i).first = pos.at(i) * 10'000;
+    info.at(i).second = velocity.at(i);
+  }
+  std::sort(info.begin(), info.end(), [](pair<ll,ll>& i1, pair<ll,ll>& i2){
+    return i1.first < i2.first;
+  });
+
+  int dist = info.back().first - info.front().first;
+
+  ll lo = 1; // 0.0001
+  ll hi = dist / info.front().second;
+
+  ll ans = hi + 1;
+  while (lo <= hi) {
+    ll mid = lo + (hi - lo) / 2;
+    if(check(info, mid)) {
+      hi = mid - 1;
+      ans = min(ans, mid);
+    } else {
+      lo = mid + 1;
+    }
+  }
+
+  cout << ans / 10'000 << "." << setw(4) << setfill('0') << ans % 10'000;
+}
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+
+  int n;
+  cin >> n;
+  vector<ll> pos(n);
+  vector<ll> velocity(n);
+
+  for (int i = 0; i < n; ++i) {
+    cin >> pos.at(i);
+  }
+  for (int i = 0; i < n; ++i) {
+    cin >> velocity.at(i);
+  }
+
+  solution(pos, velocity);
+}
