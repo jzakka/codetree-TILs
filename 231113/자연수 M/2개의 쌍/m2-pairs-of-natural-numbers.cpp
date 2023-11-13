c9adc081a8593f87ea0c5fd4752d7fa1bd15b6b3
Jzakka <105845911+jzakka@users.noreply.github.com>
2023-11-13 15:42:40 +0900

    Title: 자연수 M/2개의 쌍, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 231113/자연수 M/2개의 쌍/m2-pairs-of-natural-numbers.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <queue>
+#include <set>
+#include <unordered_set>
+#include <unordered_map>
+#include <stack>
+#include <map>
+
+#define ll long long
+#define pdd pair<double,double>
+#define pii pair<int,int>
+
+using namespace std;
+
+void solution(map<int,int>& counts){
+  auto s = counts.begin();
+  auto e = prev(counts.end());
+
+  int ans = INT32_MAX;
+  while (s->first <= e->first) {
+    if (s->first == e->first) {
+      ans = min(ans, 2 * s->first);
+      s++;
+      e--;
+    } else {
+      ans = min(ans, s->first + e->first);
+
+      if (s->second < e->second) {
+        e->second -= s->second;
+        s++;
+      } else if(s->second > e->second){
+        s->second -= e->second;
+        e--;
+      } else {
+        s++;
+        e--;
+      }
+    }
+  }
+
+  cout << ans;
+}
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+
+  int n;
+  cin >> n;
+
+  map<int, int> cnts;
+
+  for (int i = 0; i < n; ++i) {
+    int num, cnt;
+    cin >> cnt >> num;
+    cnts[num] = cnt;
+  }
+
+  solution(cnts);
+}
